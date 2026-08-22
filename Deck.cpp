3813b13aad74c0e16180dc3f597567a69f5a7cf6
conectionist <dan.manson@protonmail.com>
2026-08-22 19:07:33 +0300

    implemented deck shuffle

--- Deck.cpp
+++ Deck.cpp
@@ -1,5 +1,8 @@
 #include "Deck.h"
 
+#include <algorithm>
+#include <random>
+
 void Deck::addCard(Card&& card)
 {
     deck.push_back(std::move(card));
@@ -7,6 +10,9 @@ void Deck::addCard(Card&& card)
 
 void Deck::shuffle()
 {
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::shuffle(deck.begin(), deck.end(), generator);
 }
 
 Card *Deck::getCardAt(unsigned int i)
